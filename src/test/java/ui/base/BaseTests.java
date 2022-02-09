17ce8696a560c5321edf3f8c3281290f53b45fb1
Moaaz Adel <moazadel15@gmail.com>
2022-02-09 18:58:43 +0200

    :Update Screen Resolution from 'Maximize' to '1024*768'

--- src/test/java/ui/base/BaseTests.java
+++ src/test/java/ui/base/BaseTests.java
@@ -1,6 +1,7 @@
 package ui.base;
 
 import com.github.javafaker.Faker;
+import org.openqa.selenium.Dimension;
 import ui.helpers.Helpers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -17,7 +18,8 @@ public class BaseTests extends Helpers {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1024,768));
+//        driver.manage().window().maximize();
         driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
     }
 
